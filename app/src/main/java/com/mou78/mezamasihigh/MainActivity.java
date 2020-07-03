01487c760143a82914c2e285fd5f3a157de00374
ryotaro <itour433@yahoo.co.jp>
2020-07-03 20:35:21 +0900

    Initial commit

---
+++ app/src/main/java/com/mou78/mezamasihigh/MainActivity.java
@@ -0,0 +1,20 @@
+package com.mou78.mezamasihigh;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlarmManager;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+    public void changetime(View v){
+
+    }
+}
