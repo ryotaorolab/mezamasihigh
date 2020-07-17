82ae073b26553288cb75f79edbb45576fe348c2c
ryotaro <itour433@yahoo.co.jp>
2020-07-17 20:33:18 +0900

    change3

--- app/src/main/java/com/mou78/mezamasihigh/MainActivity.java
+++ app/src/main/java/com/mou78/mezamasihigh/MainActivity.java
@@ -2,13 +2,9 @@ package com.mou78.mezamasihigh;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.AlarmManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
-
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
 
     //時間を設定する画面に移動
     public void changebutton(View v){
-        Intent intent = new Intent(this, changetime.class);
+        Intent intent = new Intent(this, ChangeTimeActivity.class);
         startActivity(intent);
     }
 }
